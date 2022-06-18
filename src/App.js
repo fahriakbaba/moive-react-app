0b4bb56e6e7431f56bd9b90b1dcb811414bd5855
Fahri Akbaba <fahriakbaba@gmail.com>
2022-06-18 20:04:18 +0300

    css added and updated

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import MovieCard from './MovieCard';
 
 const API_key = "334af70c";
 function App() {
-  const [data, setData] = React.useState([]);
+  const [movies, setMovies] = React.useState([]);
   const [search, setSearch] = React.useState("");
 
   React.useEffect(() => {
-    getMovies("spiderman");
+    getMovies("batman");
   }, [])
   async function getMovies(searchMovie) {
     const res = await fetch(`http://www.omdbapi.com/?apikey=${API_key}&s=${searchMovie}`);
     const data = await res.json();
-    setData(data.Search)
+    setMovies(data.Search)
   }
 
-  function handleClick() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!search) {
+      alert("enter your movie name");
+      return
+    }
     getMovies(search);
     setSearch("");
   }
   return (
     <div className="App">
       <h1>MovieLand</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder='Enter your movie name'
@@ -30,13 +35,19 @@ function App() {
           value={search}
           onChange={e => setSearch(e.target.value)}
         />
-        <button type='button' className='search-btn' onClick={handleClick}>search</button>
+        <button type='submit' className='search-btn'>search</button>
       </form>
-      <div className="movie-container">
-        {
-          data.map((movie, index) => (<MovieCard key={index} {...movie} />))
-        }
-      </div>
+      {movies.length > 0 ? (
+        <div className="movie-container">
+          {
+            movies.map((movie, index) => (<MovieCard key={index} {...movie} />))
+          }
+        </div>
+      ) : (
+        <h2 style={{color:"white"}} >No movies found</h2>
+      )
+      }
+
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 
 function MovieCard({Poster, Title, Year, Type}) {
 
-    const imageCSS ={
-        width:"310px",
-        height:"460px",
-        margin: "1.5rem"
-    }
+    
   return (
-    <section className='moviecard-container'>
-        <h4>{Year}</h4>
-        <img style={imageCSS} src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
-        <h4>{Type}</h4>
-        <p>{Title}</p>
+    <section className='movie'>
+        <h4 className='movie-year'>{Year}</h4>
+        <img className='movie-image' src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+        <h4 className='movie-type'>{Type}</h4>
+        <p className='movie-title'>{Title}</p>
     </section>
   )
 }
